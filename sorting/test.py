f81e45cd70c9bace3e9dc40f1669649fbe6539e0
korobov <korobovms94@gmail.com>
2014-07-11 14:26:40 +0400

    Insertion sort added

--- sorting/test.py
+++ sorting/test.py
@@ -1,11 +1,12 @@
 from heapsort import heapsort
 from mergesort import mergesort
+from insertion_sort import insertion_sort
 import unittest
 import random
 
 class SortingTest(unittest.TestCase):
     def setUp(self):
-        self.sort_functions = [heapsort, mergesort]
+        self.sort_functions = [heapsort, mergesort, insertion_sort]
 
     def test_empty(self):
         for sort_func in self.sort_functions:
@@ -15,8 +16,8 @@ class SortingTest(unittest.TestCase):
 
     def test_random(self):
         sizes = [10 for x in range(10)]
-        for size in sizes:
-            for sort_func in self.sort_functions:
+        for sort_func in self.sort_functions:
+            for size in sizes:
                 source = [random.random() for i in range(size)]
                 excpected = sorted(source)
                 actual = source[:]
